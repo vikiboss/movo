9673737928c2d83e343c7c85d5a826a0260def47
Viki <hi@viki.moe>
2023-01-30 17:47:49 +0800

    fix: fix android subid low error

--- src/core/device.ts
+++ src/core/device.ts
@@ -108,18 +108,18 @@ export enum Platform {
 export type Apk = typeof mobile
 
 const mobile = {
-	id: "com.tencent.mobileqq",
-	name: "A8.8.80.7400",
-	version: "8.8.80.7400",
-	ver: "8.8.80",
-	sign: Buffer.from([166, 183, 69, 191, 36, 162, 194, 119, 82, 119, 22, 246, 243, 110, 182, 141]),
-	buildtime: 1640921786,
-	appid: 16,
-	subid: 537113159,
-	bitmap: 184024956,
-	sigmap: 34869472,
-	sdkver: "6.0.0.2494",
-	display: "Android",
+  id: 'com.tencent.mobileqq',
+  name: 'A8.8.80.7400',
+  version: '8.8.80.7400',
+  ver: '8.8.80',
+  sign: Buffer.from([166, 183, 69, 191, 36, 162, 194, 119, 82, 119, 22, 246, 243, 110, 182, 141]),
+  buildtime: 1640921786,
+  appid: 16,
+  subid: 537143609,
+  bitmap: 184024956,
+  sigmap: 34869472,
+  sdkver: '6.0.0.2494',
+  display: 'Android'
 }
 const watch: Apk = {
 	id: "com.tencent.qqlite",
